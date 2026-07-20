afb2794931d5c623a216f96aed45e2625787fd9d
marino <marino.menendez@gmail.com>
2026-07-20 12:23:18 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'clientesApp_React_Mongo',
+  webDir: 'dist'
+};
+
+export default config;
